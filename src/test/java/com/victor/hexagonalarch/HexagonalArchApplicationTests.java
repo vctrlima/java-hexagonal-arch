4fa12d6069ba92a25157bac6d56c69bd5ea5c06a
Victor Lima <victor.limac@icloud.com>
2024-05-12 16:35:03 -0300

    chore: initial commit

---
+++ src/test/java/com/victor/hexagonalarch/HexagonalArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.hexagonalarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
